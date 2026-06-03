6cf77ecfecced739bd4aae0fe0bb0ecf7fb19645
ThiagoRelias <thiagorelias04052007@gmail.com>
2026-06-03 11:48:46 -0300

    feat: Adicionando email, telefone

--- form-web/js/script.js
+++ form-web/js/script.js
@@ -1,17 +1,30 @@
 const nome = document.getElementById("nome");
+const telefone = document.getElementById("telefone");
+const email = document.getElementById("email");
 const button = document.getElementById("button");
 const lista = document.getElementById("lista");
 
-
 button.addEventListener("click", (event) => {
    event.preventDefault();
-   const input = nome.value;
-   const templateHTML = `<li>${input}</li>`;
-
+   
+   const inputNome = nome.value;
+   const inputTelefone = telefone.value;
+   const inputEmail = email.value;
    
 
-   // incluir itens no HTML
+const templateHTML = `
+   <li class="item-usuario">
+      <strong>Nome:</strong> ${inputNome} <br>
+      <strong>Telefone:</strong> ${inputTelefone} <br>
+      <strong>E-mail:</strong> ${inputEmail}
+   </li>
+`;
+
+
+   // Inclui o item no HTML
    lista.innerHTML += templateHTML;
 
    nome.value = "";
+   telefone.value = "";
+   email.value = "";
 });
