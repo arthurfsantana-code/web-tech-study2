7c53a23abd20d462a1e54ebe3bffb513867e317d
ThiagoRelias <thiagorelias04052007@gmail.com>
2026-06-03 11:17:47 -0300

    feat:criando javascript

---
+++ form-web/js/script.js
@@ -0,0 +1,12 @@
+const nome = document.getElementById("nome");
+const button = document.getElementById("button");
+const lista = document.getElementById("lista");
+
+
+button.addEventListener("click", (event) => {
+   event.preventDefault();
+
+   console.log(nome.value)
+
+   // incluir itens no HTML
+})
