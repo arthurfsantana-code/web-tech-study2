2e26ae189f8dc3343408597588de25fda6c8333f
ThiagoRelias <thiagorelias04052007@gmail.com>
2026-06-08 11:20:48 -0300

    feat:adicionando codigo

---
+++ form-web/Ex.js/calc.js
@@ -0,0 +1,17 @@
+// Usando let (se precisar alterar o valor no futuro)
+let num1 = 10;
+let num2 = 10;
+
+// Operações
+let soma = num1 + num2;
+let divisao = num1 / num2;
+let multiplicacao = num1 * num2;
+let subtracao = num1 - num2;
+
+//Resultados
+console.log("A soma é: " + soma);
+console.log("divisão: " + divisao);
+console.log("multiplicação: " + multiplicacao);
+console.log("subtração: " + subtracao);
+
+
