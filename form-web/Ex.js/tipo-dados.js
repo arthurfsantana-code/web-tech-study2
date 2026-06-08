513cd497a1fcc1c5265d626bd12ba07d559ad3dd
ThiagoRelias <thiagorelias04052007@gmail.com>
2026-06-08 10:46:00 -0300

    feat:adicionando codigos novos

---
+++ form-web/Ex.js/tipo-dados.js
@@ -0,0 +1,18 @@
+//string 
+let nome = "Thiago Ribeiro";
+
+//inteiro
+let idade = 18;
+
+//booleano
+let casado = false;
+
+//Numeros flutuantes 
+let valorProduto = 19.90;
+
+console.log(" Meu nome é: " + nome);
+console.log(" Minha idade é: " + idade);
+console.log(" casado ? " + casado);
+console.log(" R$ " + valorProduto);
+console.log( typeof nome);
+
