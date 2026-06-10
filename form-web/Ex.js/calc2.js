dcec798e73d6ca554cf5b76f47067c4ec319d828
ThiagoRelias <thiagorelias04052007@gmail.com>
2026-06-10 09:05:28 -0300

    feat: Atualizacao da calc.js

---
+++ form-web/Ex.js/calc2.js
@@ -0,0 +1,21 @@
+let num1 = Number(prompt("Digite um número"));
+let num2 = Number(prompt("Digite outro número"));
+
+//Operações
+let soma = num1 + num2;
+let sub = num1 - num2; 
+let div = num1 / num2;
+let multiplicacao = num1 * num2;
+
+// Resultados no console
+console.log(soma);
+console.log(typeof soma);
+
+console.log(sub); 
+console.log(typeof sub); 
+
+console.log(div);
+console.log(typeof div);
+
+console.log(multiplicacao);
+console.log(typeof multiplicacao);
