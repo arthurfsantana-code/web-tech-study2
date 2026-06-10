44ebaf3e274785c5c32314c1225c90c04ce1ad1f
ThiagoRelias <thiagorelias04052007@gmail.com>
2026-06-10 08:45:45 -0300

    feat: adicionando func.html/func.js

---
+++ form-web/Ex.js/func.js
@@ -0,0 +1,20 @@
+function minhaFuncao() {
+    document.write("Minha Função");
+
+}
+function pularLinha () {
+    document.write("<br><br>");
+}
+
+function mostrarNome(nome) {
+    pularLinha();
+    Document.write(nome);
+    pularLinha();
+}
+
+
+minhaFuncao();
+pularLinha ();
+minhaFuncao();
+mostrarNome();
+mostrarNome();
