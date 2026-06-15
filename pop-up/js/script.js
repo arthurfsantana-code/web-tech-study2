1428a4b5238299c77be777edb9a705ce55b63222
ThiagoRelias <thiagorelias04052007@gmail.com>
2026-06-15 10:43:31 -0300

    feat:finalizando modal em js

--- pop-up/js/script.js
+++ pop-up/js/script.js
@@ -1,1 +1,17 @@
-alert("pop-up");
+const button = document.querySelector("button");
+const popup = document.querySelector(".popup-wrapper");
+
+button.addEventListener("click", function(event) {
+    popup.classList.add("d-block");
+});
+
+popup.addEventListener("click", function(event) {
+    const classOfClickElement = event.target.classList[0];
+    const ClassNameListArray = ["popup-close", "popup-link", "popup-wrapper"];
+
+    console.log(ClassNameListArray.includes(classOfClickElement));
+
+    if(ClassNameListArray.includes(classOfClickElement)) {
+        popup.classList.remove("d-block")
+    };
+});
