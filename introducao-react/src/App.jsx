2889c042c8d29f522294555878f2ab9a8d3aac14
ThiagoRelias <thiagorelias04052007@gmail.com>
2026-08-19 10:41:55 -0300

    feat: adicionando componente ImgCard

--- introducao-react/src/App.jsx
+++ introducao-react/src/App.jsx
@@ -2,6 +2,8 @@ import Header from './Components/Header.jsx'
 import Card from './Components/Card.jsx'
 import Banner from './Components/Banner.jsx'
 import Footer from './Components/footer.jsx'
+import ImgCard from './Components/ImgCard.jsx'
+import './Components/Img.css'
 
 
 
@@ -14,11 +16,14 @@ function App() {
   
     <Header title="Introdução ao React" />
 
+    <ImgCard caption="Lorem ipsum dolor sit amet," />
+
+    
+
     <Banner>
       <h1>Bem-Vindo ao meu site</h1>
        <p>Aqui você encontrará conteúdos sobre desenvolvimento web.</p>
     </Banner>
-
       <Card />
       <Footer title="new Lorem ipsum dolor sit amet, consectetur adipiscing elit."/> 
      
